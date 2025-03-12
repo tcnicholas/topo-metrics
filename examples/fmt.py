1d9345e04ad8ffc9561d19256d7af636f4bc0797
Thomas Nicholas <tcnicholas@me.com>
2025-03-12 17:30:57 +0100

    examples

--- examples/fmt.py
+++ examples/fmt.py
@@ -1,3 +1,6 @@
+from pathlib import Path
+
+
 def header(example_name: str) -> None:
     """ Print a header for the example. """
 
@@ -12,3 +15,10 @@ def footer() -> None:
     print("\n" + "-" * 15)
     print("End of analysis")
     print("-" * 15, end="\n\n")
+
+
+def options() -> list[str]:
+    """ Auto-find all .cgd files in the data directory. """
+
+    data_dir = Path(__file__).parent / "data"
+    return [f.stem for f in data_dir.glob("*.cgd")]
--- examples/main.py
+++ examples/main.py
@@ -1,23 +1,25 @@
+import argparse  # noqa: I001
 from pathlib import Path
 
 from topo_metrics import Topology
 
-from .fmt import footer, header
+from fmt import footer, header, options  # noqa: I001
 
 
-def main():
+def main(struct_name: str):
     """ Quick analysis of the alpha-cristobalite (dia) structure. """
 
-    header("Alpha-cristobalite structure analysis")
+    header(f"{struct_name} structure analysis")
 
     # load the alpha-cristobalite structure from a .cgd file.
-    structure_file = Path(__file__).parent / "data/alpha-cristobalite.cgd"
+    structure_file = Path(__file__).parent / f"data/{struct_name}.cgd"
     topology = Topology.from_cgd(structure_file)
     print("This network contains:\n\n", topology, end="\n\n")
 
-    # check topology.
-    topology_code = topology.get_topological_genome()
-    print("It has topology code:\n\n", topology_code.strip(), end="\n\n")
+    # check topology if not too large (i.e., don't apply to amorphous networks).
+    if len(topology.nodes) < 400:
+        topology_code = topology.get_topological_genome()
+        print("It has topology code:\n\n", topology_code.strip(), end="\n\n")
 
     # check rings.
     rings = topology.get_rings(depth=6)
@@ -36,5 +38,13 @@ def main():
 
     footer()
 
+
 if __name__ == "__main__":
-    main()
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "struct_name", 
+        help="The name of the structure to analyse.",
+        choices=options()
+    )
+    args = parser.parse_args()
+    main(args.struct_name)
