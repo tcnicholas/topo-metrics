5f54a1e79cc63d647d448b476d503af9b50b260c
Thomas Nicholas <tcnicholas@me.com>
2025-03-23 09:27:22 +0100

    bump 0.1.0 -> 0.1.1

--- src/topo_metrics/__init__.py
+++ src/topo_metrics/__init__.py
@@ -8,7 +8,7 @@ from topo_metrics.rings import Ring
 from topo_metrics.topology import Topology
 
 __all__ = ["Topology", "Ring", "Cluster"]
-__version__ = "0.1.0"
+__version__ = "0.1.1"
 
 
 def _setup_julia():
